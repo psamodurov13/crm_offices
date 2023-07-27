9dbdbf9459284045be1f2cb84f9c0da9dd911127
psamodurov13 <psamodurov13@gmail.com>
2023-07-27 22:29:59 +0300

    reqdy to deploy

--- crm_offices/crm_offices/settings.py
+++ crm_offices/crm_offices/settings.py
@@ -143,9 +143,15 @@ MEDIA_ROOT = BASE_DIR / 'media'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+    production = False
+except ImportError:
+    from .prod_settings import *
+    production = True
+
