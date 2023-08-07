fc08b4d0d6b7e46f8b1ffb69b48230c912f87669
psamodurov13 <psamodurov13@gmail.com>
2023-08-07 11:36:58 +0300

    added expenses types and currencies creation to dockerfile

--- crm_offices/create_superuser_if_not_exists.py
+++ crm_offices/create_superuser_if_not_exists.py
@@ -5,6 +5,26 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crm_offices.settings")
 django.setup()
 
 from django.contrib.auth.models import User
+from crm.models import *
 
 if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+
+if not Currency.objects.all():
+    currencies = [
+        ['Сум', 'Soʻm'],
+        ['Доллар США', '$']
+    ]
+    for i in currencies:
+        Currency.objects.create(
+            name=i[0],
+            symbol=i[1]
+        )
+
+
+if not ExpensesTypes.objects.all():
+    expenses_types = ['Аренда', 'Иные расходы', 'Коммунальные платежи', 'Зарплата']
+    for i in expenses_types:
+        ExpensesTypes.objects.create(
+            name=i
+        )
